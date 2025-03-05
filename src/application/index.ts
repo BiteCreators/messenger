f2d00a5215286f2d1e47a4a7dc35ed36e2b202cb
DmitryChues <dmitrychues2312@gmail.com>
2025-03-05 19:08:13 +0300

    IN-388: added api for messenger

---
+++ src/application/index.ts
@@ -0,0 +1,3 @@
+export { messagesApi } from './api/messenger.api'
+
+export { type Dialog, type Message, MessageStatus, MessageType } from './types/messenger.type'
---
+++ src/application/lib/consts.ts
@@ -0,0 +1,7 @@
+export const WS_EVENT_PATH = {
+  ERROR: 'error',
+  MESSAGE_DELETED: 'message-deleted',
+  MESSAGE_SENT: 'message-sent',
+  RECEIVE_MESSAGE: 'receive-message',
+  UPDATE_MESSAGE: 'update-message',
+}
